4041bd5ed860e32e42577b1ec8ad115db290b7e3
Mohammed-ABR <alabrashmohammed1@gmail.com>
2025-02-11 16:26:28 +0100

    add devtools-debugger

--- lesson-plans/devtools-debugger/1-isolate/exercises/2-while-loops/1-input-something.js
+++ lesson-plans/devtools-debugger/1-isolate/exercises/2-while-loops/1-input-something.js
@@ -1,12 +1,20 @@
 'use strict';
-
+/*
 let input = null;
 while (input === null) {
   input = prompt('input something:');
 }
 
 alert('thank you!');
+*/
 
 /*  ===== Challenges =====
   - Try to create an infinite loop so the user gets stuck inside your prompt.
 */
+
+let input = null;
+while (input === null) {
+  input = prompt('Enter something:');
+}
+
+alert('Thank you!');
--- lesson-plans/devtools-debugger/1-isolate/exercises/3-for-of-loops/1-doubled-characters.js
+++ lesson-plans/devtools-debugger/1-isolate/exercises/3-for-of-loops/1-doubled-characters.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 let text = null;
 while (text === null) {
   text = prompt('enter some text, each character will be doubled:');
@@ -11,8 +12,31 @@ for (let nextChar of text) {
 }
 
 alert(doubled);
+*/
 
 /*  ===== Challenges =====
   - Add an hyphen after each character.
   - Add an hyphen only between the characters.
 */
+
+let text = null;
+while (text === null) {
+  text = prompt('Enter some text, each character will be doubled:');
+}
+
+let doubledWithHyphens = '';
+for (let nextChar of text) {
+  doubledWithHyphens = doubledWithHyphens + nextChar + nextChar + '-';
+}
+alert(doubledWithHyphens);
+
+let doubledWithHyphensOnlyBetween = '';
+
+for (let i = 0; i < text.length; i++) {
+  doubledWithHyphensOnlyBetween = text[i] + text[i];
+  if (i < text.length - 1) {
+    doubledWithHyphensOnlyBetween += '-';
+  }
+}
+
+alert(doubledWithHyphensOnlyBetween);
